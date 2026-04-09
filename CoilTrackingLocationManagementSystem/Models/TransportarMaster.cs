76fad305c108823eaa015a1e79dbfa9dca45770b
Shital Bawage <Shital Bawage@DESKTOP-5VQ610R>
2026-04-09 14:44:31 +0530

    Added new model transportar master

---
+++ CoilTrackingLocationManagementSystem/Models/TransportarMaster.cs
@@ -0,0 +1,12 @@
+﻿namespace CoilTrackingLocationManagementSystem.Models
+{
+    public class TransportarMaster
+    {
+        public int Id { get; set; }
+        public string TransportorName { get; set; } = string.Empty;
+        public string ContactNumber { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
