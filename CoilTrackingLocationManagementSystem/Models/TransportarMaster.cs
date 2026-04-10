5c0bbe4bbe89791012fd6de80f201d09a7188f4f
Shital Bawage <Shital Bawage@DESKTOP-5VQ610R>
2026-04-10 12:15:07 +0530

    modified archana

--- CoilTrackingLocationManagementSystem/Models/TransportarMaster.cs
+++ CoilTrackingLocationManagementSystem/Models/TransportarMaster.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string TransportorName { get; set; } = string.Empty;
         public string ContactNumber { get; set; } = string.Empty;
+        public string ContactPersonName { get; set; } = string.Empty; // Added ContactPersonName property by Archana on 2024-06-17
+        public string ContactPersonEmailId { get; set; } = string.Empty; // Added ContactPersonEmailId property by Archana on 2024-06-17
         public bool IsActive { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
